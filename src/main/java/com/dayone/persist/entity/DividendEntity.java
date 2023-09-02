172d2fef1e60522686e290fb7982aab0517d70ab
HanSeul <w0w1278@naver.com>
2023-09-02 19:42:52 +0900

    회사명으로 배당금 조회

--- src/main/java/com/dayone/persist/entity/DividendEntity.java
+++ src/main/java/com/dayone/persist/entity/DividendEntity.java
@@ -20,13 +20,13 @@ public class DividendEntity {
     private Long id;
 
 //    @Column(unique = true)
-    private Long company_id;
+    private Long companyId;
 
     private LocalDateTime date;
     private String dividend;
 
     public DividendEntity(Long companyId, Dividend dividend) {
-        this.company_id = companyId;
+        this.companyId = companyId;
         this.date = dividend.getDate();
         this.dividend = dividend.getDividend();
     }
---
+++ src/main/java/com/dayone/service/FinanceService.java
@@ -0,0 +1,56 @@
+package com.dayone.service;
+
+import com.dayone.model.Company;
+import com.dayone.model.Dividend;
+import com.dayone.model.ScrapedResult;
+import com.dayone.persist.entity.CompanyEntity;
+import com.dayone.persist.entity.DividendEntity;
+import com.dayone.persist.repository.CompanyRepository;
+import com.dayone.persist.repository.DividendRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class FinanceService {
+    private CompanyRepository companyRepository;
+    private DividendRepository dividendRepository;
+
+    public ScrapedResult getDividendByCompanyName(String companyName) {
+        // 1. 회사명을 기준으로 회사 정보를 조회
+        CompanyEntity company = this.companyRepository.findByName(companyName)
+                .orElseThrow(() -> new RuntimeException("해당 이름의 회사가 없습니다."));
+
+        // 2. 조회된 회사 ID로 배당금 정보 조회
+        List<DividendEntity> dividendEntities = this.dividendRepository.findAllByCompanyId(company.getId());
+
+
+        // 3. 결과 조합 후 반환
+//        List<Dividend> dividends = new ArrayList<>();
+//        for (var entity : dividendEntities) {
+//            dividends.add(Dividend.builder()
+//                            .date(entity.getDate())
+//                            .dividend(entity.getDividend())
+//                            .build());
+//        }
+
+        List<Dividend> dividends = dividendEntities.stream()
+                .map(e -> Dividend.builder()
+                        .date(e.getDate())
+                        .dividend(e.getDividend())
+                        .build())
+                .collect(Collectors.toList());
+
+        return new ScrapedResult(
+                Company.builder()
+                        .name(company.getName())
+                        .ticker(company.getTicker()).build(),
+                dividends
+                );
+    }
+}
